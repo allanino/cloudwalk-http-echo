5594e47de7420231a6b6d6b962a52d9e8bc7cb78
allanino <allaninocencio@yahoo.com.br>
2014-10-22 15:47:47 -0200

    Added Pi joke

--- echo.rb
+++ echo.rb
@@ -13,10 +13,14 @@ end
 post '/' do
   n = Random.rand(1...35)
   salt = rand(36**n).to_s(36) # Transform to base 27 + 9 for a-z0-9
-  p salt
-  if params["anotherVariable"]
-    salt + "#{params["buf"]} #{params["anotherVariable"]}"
+  pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091"
+  if pi.include? params["buf"]
+    "You're so stupid! It's not #{params["buf"]}, it's #{pi}... (not a repeating decimal)"
   else
-    salt + "#{params["buf"]}"
+    if params["anotherVariable"]
+      salt + "#{params["buf"]} #{params["anotherVariable"]}"
+    else
+      salt + "#{params["buf"]}"
+    end
   end
 end
